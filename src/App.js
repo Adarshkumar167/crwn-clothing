e3120748f5630e840b0589bce67334ff82fa03c3
Adarsh <adarshkumar167@gmail.com>
2023-01-18 18:34:51 +0530

    77

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './categories.styles.scss'
+
 const App = () => {
 
   const categories = [
@@ -25,10 +27,10 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      {categories.map(({title}) => (
-        <div className="category-conatiner">
+      {categories.map(({title, id}) => (
+        <div key={id} className="category-container">
          <div className="background-image" />
-          <div className="category-body-conatiner">
+          <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
           </div>
