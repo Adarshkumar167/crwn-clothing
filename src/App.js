b3c589a7311f0a1483a94ee4470e1ec960d608d5
Adarsh <adarshkumar167@gmail.com>
2023-01-20 18:07:09 +0530

    84

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 
+const Shop = () => {
+  return <h1>I am the shop page</h1>
+}
+
 const App = () => {
   return (
     <Routes>
-      <Route path = '/home' element = {<Home />} />
+      <Route path = '/home' element = {<Home />}>
+        <Route path = 'shop' element = {<Shop />} />
+      </Route>
     </Routes>
   );
 };
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,3 +1,5 @@
+import { Outlet } from 'react-router-dom';
+
 import Directory from '../../components/directory/directory.component'
 
 const Home = () => {
@@ -31,7 +33,10 @@ const Home = () => {
   ];
 
   return (
-    <Directory categories={categories} />
+    <div>
+      <Directory categories={categories} />
+      <Outlet/>
+    </div>
   );
 };
 
