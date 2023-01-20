b3885016f1676b9ec876e494f8566c94785d518f
Adarsh <adarshkumar167@gmail.com>
2023-01-20 22:45:15 +0530

    95_signin

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/f
 const SignIn = () => {
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
-        createUserDocumentFromAuth(user);
+        const userDocRef = await createUserDocumentFromAuth(user);
     }
 
     return (
