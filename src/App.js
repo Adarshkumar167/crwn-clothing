b898ed86caa97594280daf1dd132afe44ca65d3a
Adarsh <adarshkumar167@gmail.com>
2023-01-20 18:27:48 +0530

    86

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
+import Navigation from './routes/navigation/navigation.component';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>
