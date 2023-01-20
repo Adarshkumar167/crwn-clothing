2ce29aeaa39ebccfb96d58689f7e899634d1e288
Adarsh <adarshkumar167@gmail.com>
2023-01-20 18:16:12 +0530

    85

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
 const Shop = () => {
   return <h1>I am the shop page</h1>
-}
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path = '/home' element = {<Home />}>
+      <Route path = '/' element = {<Navigation />}>
+        <Route index element = {<Home />} />
         <Route path = 'shop' element = {<Shop />} />
       </Route>
     </Routes>
